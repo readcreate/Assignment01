3c7e034c79a62f31e5343cc495faff2d36462938
at143 <anshulkumar@yahoo.com>
2024-02-20 23:50:13 -0500

    end of assignment

---
+++ js/script.js
@@ -0,0 +1,143 @@
+// Anshul Kumar Module 2 Assignment 1, COMP 690 Ruvalcaba Spr 2024 SDCCE
+
+// STEP 1 - convert to camel case
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// STEP 2 - num, str, boolean, null literal expressions
+// 3.4 // numeric
+// 'Traffic lights are annoying but useful' // string
+// true // boolean
+// null // null
+
+// STEP 3 - two complex or variable expressions
+// let numBananas = 4
+// let myLabel = "This is where we keep the extra barbecue sauce"
+
+// STEP 4 - declare variables
+// let strFirstName
+// let strLastName
+// let strAddress
+// let strCity
+// let strState
+// let intZipCode // initially I had numZipCode but then changed to intZipCode after seeing lecture slides
+// let intYourAge // same num vs int decision
+// let strReferralSource
+// let strMayWeContactYou
+
+// STEP 5 - declare and assign values 3 ways
+
+// 1st way
+// console.log('1st way')
+// let strFirstName
+// strFirstName = 'Aleeesha'
+
+// 2nd way
+// console.log('2nd way')
+// let strCity = 'JavaScriptVille'
+
+// 3rd way
+// console.log('3rd way')
+// let intZipCode = 92022, strMayWeContactYou = 'Only if it\'s very important'
+
+// STEP 6 - add and display coerced number and string
+// let myStringToDisplay
+// myStringToDisplay = 'The magic number is sometimes ' + 329
+// console.log(myStringToDisplay)
+
+// STEP 7 - more coerced results
+// let firstVarBlnStr, secondVarNumBln
+
+// firstVarBlnStr = 'Sometimes teeth are ' + false
+// console.log(firstVarBlnStr)
+
+// secondVarNumBln = 5 + true
+// console.log(secondVarNumBln)
+
+// STEP 8 - fix string literal
+
+// No, the string literal is not correct in its original form. Here is the correction:
+// let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+// console.log(someString)
+
+// STEP 9 - create null and undefined variable values
+
+// let thisWillBeNull = null
+// console.log(thisWillBeNull)
+
+// let thisWillBeUndefined
+// console.log(thisWillBeUndefined)
+
+// STEP 10 - return types with typeof unary operator
+
+// console.log(typeof('This is a string'))
+// console.log(typeof(10002))
+// console.log(typeof(false))
+// console.log(typeof(new Object()))
+// console.log(typeof(thisIsUndefinedAdalfkjdafhaefolkdskjweoi))
+
+// STEP 11 - concatenated alert
+// alert("Hello" + " Anshul Kumar" + ", welcome to the JavaScript class!")
+
+// STEP 12 - substitute name variable
+// let name = 'Anshul Kumar'
+// alert(`Hello ${name}, welcome to the JavaScript class!`)
+
+// STEP 13 - use course variable for course name
+// let name = 'Anshul Kumar'
+// let course = 'JavaScript'
+// alert(`Hello ${name}, welcome to the ${course} class!`)
+
+// STEP 14 - add line break
+// let name = 'Anshul Kumar'
+// let course = 'JavaScript'
+// alert(`Hello ${name}.\nWelcome to the ${course} class!`)
+
+// STEP 15 - ask for name in prompt
+// let name = prompt('What is your name?')
+// let course = 'JavaScript'
+// alert(`Hello ${name}.\nWelcome to the ${course} class!`)
+
+// STEP 16 - ask for course in prompt
+// let name = prompt('What is your name?')
+// let course = prompt('What class are you taking?')
+// alert(`Hello ${name}.\nWelcome to the ${course} class!`)
+
+// STEP 17 - display sum of x and y
+// let x = 12, y = 2300000000
+// console.log(x+y)
+
+// STEP 18 - add and assign
+// let x = 20
+// x += 20
+// console.log(x)
+
+// STEP 19 - multiply and assign
+// let x = 20
+// x *= 5
+// console.log(x)
+
+// STEP 20 - modulus then divide and assign
+// let x = 20 % 3
+// x /= 1
+// console.log(x)
+
+// STEP 21 - application with comparison and logical operators that evaluates to true
+let numberA = 4
+let numberB = 4
+let numberThatBothOtherNumbersMustEqual = 4
+let myResult = (numberThatBothOtherNumbersMustEqual === numberA) && (numberThatBothOtherNumbersMustEqual === numberB)
+console.log(myResult)
+
+// STEP 22 - application with different comparison and logical operators that evaluates to false
+let numberC = 3
+let numberD = 7
+let numberThatAtLeastOneOtherNumberMustBeGreaterThan = 184
+let myOtherResult = (numberThatAtLeastOneOtherNumberMustBeGreaterThan < numberC) || (numberThatAtLeastOneOtherNumberMustBeGreaterThan < numberD)
+console.log(myOtherResult)
+
+
+// document.write("test")
